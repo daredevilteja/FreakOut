b3b512132f05566e0a22c576d78f4a68438e9fe4
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2020-12-04 12:32:52 +0530

    added basic structure for the app

---
+++ src/Main/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the sample body</p>
+      </div>
+    );
+  }
+}
