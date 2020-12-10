a20887ba8beaea96a2aa924082e6349248d6096d
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2020-12-10 17:58:10 +0530

    login page styling done

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import "./styles.css";
 
 export default class Main extends Component {
   render() {
     return (
-      <div>
-        <p>This is the sample body</p>
+      <div className="main">
+        <div className="image">
+          <h2 style={{ fontSize: "xx-large" }}>Freak Out</h2>
+          <p style={{ fontSize: "x-large" }}>
+            Freak Out helps you to store and share your memories
+          </p>
+        </div>
+        <div className="login">
+          <form className="form">
+            <input type="email" placeholder="Enter your Email"></input>
+            <input type="password" placeholder="Password"></input>
+            <input type="button" value="Log in"></input>
+            <input type="button" value="Sign Up"></input>
+          </form>
+        </div>
       </div>
     );
   }
