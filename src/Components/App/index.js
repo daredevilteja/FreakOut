bd39a7382bff84746ffbab169bc36134df21bbf1
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2020-12-23 14:50:55 +0530

    Added a state changer

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,12 +2,14 @@ import "./styles.css";
 import Header from "../Header";
 import Main from "../Main";
 import Footer from "../Footer";
+import { useState } from "react";
 
 function App() {
+  const [change, setChange] = useState(false);
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main change={change} />
       <Footer />
     </div>
   );
