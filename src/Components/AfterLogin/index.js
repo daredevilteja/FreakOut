04344e3882e6965377ce0c99622416f96ebc94e4
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2020-12-23 16:54:05 +0530

    Added textarea and buton

--- src/Components/AfterLogin/index.js
+++ src/Components/AfterLogin/index.js
@@ -1,19 +1,51 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import Posts from "../Posts";
 import "./styles.css";
 
-export default class AfterLogin extends Component {
-  render() {
-    return (
-      <div className="container-main">
-        <aside>
-          <ul>
-            <li>Home</li>
-            <li>My Posts</li>
-            <li>Profile</li>
-          </ul>
-        </aside>
-        <div className="posts"></div>
+export default function AfterLogin() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const addItem = () => {
+    items.push(newItem);
+    setItems([...items]);
+    setNewItem("");
+  };
+
+  const newItemChanged = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  return (
+    <div className="container-main">
+      <aside>
+        <ul>
+          <li>Home</li>
+          <li>My Posts</li>
+          <li>Profile</li>
+        </ul>
+      </aside>
+      <div className="posts">
+        <div>
+          <textarea
+            placeholder="Enter your post"
+            onChange={newItemChanged}
+            value={newItem}
+          ></textarea>
+          <button onClick={addItem}>Post</button>
+        </div>
+        <div>
+          {items.map((val, idx) => (
+            <Posts
+              item={val}
+              key={`${val}_${idx}`}
+              idx={idx}
+              // editHandler={}
+              // deleteHandler={}
+            />
+          ))}
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
