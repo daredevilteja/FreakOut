e6df6d39239820ec8f63711393a8173d55efccb1
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2020-12-31 20:17:51 +0530

    Login and signup handlers added

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -5,8 +5,54 @@ import Login from "../Login";
 import AfterLogin from "../AfterLogin";
 
 function App() {
-  const [change, setChange] = useState(true);
-  return <>{change ? <Login /> : <AfterLogin />}</>;
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState(undefined);
+
+  const signupHandler = (userName, password) => {
+    loginOrSignUp("http://localhost:9999/signup", userName, password);
+  };
+
+  const loginHandler = (userName, password) => {
+    loginOrSignUp("http://localhost:9999/login", userName, password);
+  };
+
+  const loginOrSignUp = (url, userName, password) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ email: userName, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((r) => {
+        if (r.ok) {
+          return { success: true };
+        } else {
+          return r.json();
+        }
+      })
+      .then((r) => {
+        if (r.success === true) {
+          setLoggedIn(true);
+        } else {
+          setError(r.err);
+        }
+      });
+  };
+
+  return (
+    <>
+      {!loggedIn ? (
+        <Login
+          signupHandler={signupHandler}
+          loginHandler={loginHandler}
+          error={error}
+        />
+      ) : (
+        <AfterLogin />
+      )}
+    </>
+  );
 }
 
 export default App;
