6f018c7d855c3df4c4431ab7402b21c01b6b96b7
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-01-06 22:04:25 +0530

    Corrected profile page display

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -27,23 +27,34 @@ export default function Profile(props) {
       });
   }, []);
 
+  const isNullOrUndefined = (val) =>
+    val === null || val === undefined || val === "";
+
   const formHandler = async (e) => {
     e.preventDefault();
 
-    setUserName(e.target[0].value);
-    setDob(e.target[1].value);
-    setSex(e.target[2].value);
-    setCountry(e.target[3].value);
-    setPhnum(e.target[4].value);
+    const values = [setUserName, setDob, setSex, setCountry, setPhnum];
+
+    values.forEach((val, idx) => {
+      if (!isNullOrUndefined(e.target[idx].value)) {
+        val(e.target[idx].value);
+      }
+    });
+
+    // setUserName(e.target[0].value);
+    // setDob(e.target[1].value);
+    // setSex(e.target[2].value);
+    // setCountry(e.target[3].value);
+    // setPhnum(e.target[4].value);
 
     await fetch("http://localhost:9999/profile", {
       method: "PUT",
       body: JSON.stringify({
-        userName: e.target[0].value,
-        dob: e.target[1].value,
-        sex: e.target[2].value,
-        country: e.target[3].value,
-        phNum: e.target[4].value,
+        userName: e.target[0].value ? e.target[0].value : username,
+        dob: e.target[1].value ? e.target[1].value : dob,
+        sex: e.target[2].value ? e.target[2].value : sex,
+        country: e.target[3].value ? e.target[3].value : country,
+        phNum: e.target[4].value ? e.target[4].value : phnum,
       }),
       headers: {
         "Content-Type": "application/json",
